cba7b202a9c54044692f4114ba14d0c74ee1c561
Андрей Катюшин <fess200@yandex.ru>
2025-06-23 01:51:58 +0300

    fgfg

---
+++ signature_script.js
@@ -0,0 +1,24 @@
+const myElement = document.getElementById('top_content');
+
+var oldX = 0;
+const resizeObserver = new ResizeObserver(entries => {
+    for (let entry of entries) {
+        const address = document.getElementById('address');
+        const newRect = address.getBoundingClientRect();
+        console.log(newRect);
+        let newX = newRect.x;
+        if (oldX != newX) {
+            const addressTitle = document.getElementById('address_title');
+            if (newX == 8) {
+                console.log('1');
+                addressTitle.style.paddingTop = "20px";
+            } else {
+                console.log('2');
+                addressTitle.style.paddingTop = "50px";
+            }
+        }
+        oldX = newX;
+    }
+});
+
+resizeObserver.observe(myElement);
