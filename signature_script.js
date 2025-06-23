d057dab1ee50e76832d8cd15a5da6d59b4fe7848
Андрей Катюшин <fess200@yandex.ru>
2025-06-23 11:40:56 +0300

    sdfsdf

--- signature_script.js
+++ signature_script.js
@@ -1,24 +1,28 @@
 const myElement = document.getElementById('top_content');
 
-var oldX = 0;
-const resizeObserver = new ResizeObserver(entries => {
-    for (let entry of entries) {
-        const addressData = document.getElementById('address_data');
-        const newRect = addressData.getBoundingClientRect();
-        console.log(newRect);
-        let newX = newRect.x;
-        if (oldX != newX) {
-            const addressTitle = document.getElementById('address_title');
-            if (newX == 8) {
-                console.log('1');
-                addressTitle.style.paddingTop = "20px";
-            } else {
-                console.log('2');
-                addressTitle.style.paddingTop = "31px";
-            }
+function updateTopContentBlockLocation(element, topChildElement, oldValueX, paddingTopOrigin) {
+    const newValue = element.getBoundingClientRect();
+    let newValueX = newValue.x;
+    if (oldValueX != newValueX) {
+        if (newValueX <= 20) {
+            topChildElement.style.paddingTop = "20px";
+        } else {
+            topChildElement.style.paddingTop = paddingTopOrigin;
         }
-        oldX = newX;
     }
+    return newValueX;
+}
+
+var oldAddressX = 0;
+var oldContactX = 0;
+const resizeObserver = new ResizeObserver(entries => {
+    const addressData = document.getElementById('address_data');
+    const addressTitle = document.getElementById('address_title');
+    oldAddressX = updateTopContentBlockLocation(addressData, addressTitle, oldAddressX, "31px");
+
+    const contactData = document.getElementById('contact_data');
+    const contactName = document.getElementById('contact_name');
+    oldContactX = updateTopContentBlockLocation(contactData, contactName, oldContactX, "0px");
 });
 
 resizeObserver.observe(myElement);
